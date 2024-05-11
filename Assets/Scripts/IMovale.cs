9a3a40792afccf24ce9ae1f9be917dce6acf26d7
Aravindan <142475619+Arav1ndan@users.noreply.github.com>
2024-05-11 13:09:39 +0530

    Car Running

---
+++ Assets/Scripts/IMovale.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public interface IMovable
+{
+   public void GoForward();
+   public void GoBack();
+}
---
+++ Assets/Scripts/ITurnable.cs
@@ -0,0 +1,8 @@
+
+using UnityEngine;
+
+public interface ITurnable 
+{
+   public void TurnLeft();
+   public void TurnRight();
+}
